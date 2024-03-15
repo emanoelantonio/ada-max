e91b3e4298d9495549293571cf1838360e996053
emanoelantonio <dev.emanoel@protonmail.com>
2024-03-15 09:46:48 -0300

    refactor: fix Avatar image

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -5,7 +5,7 @@ import * as S from "./styles";
 type AvatarProps = {
   image?: string;
   isEdit?: boolean;
-  size?: "medium" | "large";
+  size?: "medium" | "large" | "small";
   disabled?: boolean;
 };
 
@@ -15,12 +15,13 @@ export const Avatar = ({
   size = "medium",
   disabled,
 }: AvatarProps) => {
+  const imagePath = image ? `/images/${image}.webp` : user;
   return (
     <S.Container
       size={size}
       disabled={disabled}
     >
-      <img src={image || user} alt="" />
+      <img src={imagePath} alt="" />
       {isEdit && (
         <div className="icon">
           <IconEdit />
